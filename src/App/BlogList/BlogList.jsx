1e10dbcf948c5d76922b8cc75c0dc5e14f828c66
bokiv <borislavivanisevic@gmail.com>
2021-09-02 20:37:10 +0200

    Create authors page

--- src/App/BlogList/BlogList.jsx
+++ src/App/BlogList/BlogList.jsx
@@ -1,6 +1,7 @@
 import React  from "react";
 import './BlogList.css';
 import {getData} from '../../services/services'; 
+<<<<<<< HEAD
 import {useState, useEffect} from 'react'
 import {Blog} from '../Blog/Blog'
 
@@ -25,6 +26,42 @@ export const BlogList = () => {
           {posts.map( (post, userId) => (
             <Blog post={post} key={userId}/>
           ))}
+=======
+// import {useState, useEffect} from 'react'
+
+export class BlogList extends Component {
+    constructor(props) {
+      super(props)
+      this.state = {
+        blogs: []
+      }
+    }
+
+
+
+componentDidMount(){
+  getData()
+  .then(blogs => {
+    this.setState({blogs})
+  })
+}
+
+
+    render () {
+   
+      return (
+          <div className="post-container">
+            <h2>POSTS</h2>
+          {this.state.blogs.map( (element) => {
+           return ( 
+              <div> 
+                
+              <h3>{element.title}</h3>
+              <p>{element.body}</p>
+            </div>)
+          })
+          }
+>>>>>>> 7e94171c8344df92134aaeeff5e0b308a6ba1a7e
           </div>
       );
   
--- src/services/services.js
+++ src/services/services.js
@@ -5,8 +5,13 @@ export const getData = () => {
 
     return fetch(`${endPoint}/posts`)
     .then((result) => result.json())
+<<<<<<< HEAD
     .then((posts) => {
         return posts.slice(0,4);
+=======
+    .then((blogs) => {
+        return blogs.slice(0,7);
+>>>>>>> 7e94171c8344df92134aaeeff5e0b308a6ba1a7e
     });
 };
 
